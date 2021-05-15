0d6168bc954d518b1807b6040de0e3a9047e2ea7
Ibrahim BIDI <Zizwar@users.noreply.github.com>
2021-05-15 02:08:23 +0000

    switch to danger code analyics

--- pages/_document.js
+++ pages/_document.js
@@ -27,7 +27,20 @@ export default class MyDocument extends Document {
           <link href="https://fonts.googleapis.com/css?family=Raleway:400,600,700,800,900&display=swap" rel="stylesheet"></link>
           <link href="https://fonts.googleapis.com/css?family=Work+Sans:100,200,300,400,500,600,700,800" rel="stylesheet" />
           <BaseCSS />
-
+     <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=G-XF8WRSYF8C`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-XF8WRSYF8C');
+          `
+            }}
+          />
 
         </Head>
         <body className="try">
